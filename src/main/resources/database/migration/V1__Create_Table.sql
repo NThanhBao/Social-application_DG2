2d1466d2c0bf446e6d296e7cdcf2c55ff54a87ba
Thanh Bao <ntb09019@gmail.com>
2024-03-26 10:46:36 +0700

    thanhbao

--- src/main/resources/database/migration/V1__Create_Table.sql
+++ src/main/resources/database/migration/V1__Create_Table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-                    id BINARY(36) PRIMARY KEY,
+                       id CHAR(36) PRIMARY KEY,
                     username VARCHAR(50) NOT NULL,
                     password VARCHAR(50) NOT NULL,
                     first_name VARCHAR(50) NOT NULL,
@@ -16,30 +16,30 @@ CREATE TABLE users (
 
 DROP TABLE IF EXISTS `post`;
 CREATE TABLE post (
-                    id BINARY(36) PRIMARY KEY,
+                    id CHAR(36) PRIMARY KEY,
                     title VARCHAR(50) NOT NULL,
                     body TEXT NOT NULL ,
                     status VARCHAR(100) NOT NULL ,
                     total_like INT NOT NULL ,
                     total_comment INT NOT NULL ,
                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                    created_by BINARY(36),
+                    created_by CHAR(36),
                     FOREIGN KEY (created_by) REFERENCES users(id));
 
 
 DROP TABLE IF EXISTS medias;
 CREATE TABLE medias (
-                    id BINARY(16) PRIMARY KEY,
+                    id CHAR(36) PRIMARY KEY,
                     base_name VARCHAR(50) NOT NULL,
                     public_url VARCHAR(50) NOT NULL,
-                    post_id BINARY(16),
+                    post_id CHAR(36),
                     FOREIGN KEY (post_id) REFERENCES post(id));
 
 
 DROP TABLE IF EXISTS favorites;
 CREATE TABLE favorites (
-                           post_id BINARY(16),
-                           user_id BINARY(16),
+                           post_id CHAR(36),
+                           user_id CHAR(36),
                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                            PRIMARY KEY (post_id),
                            FOREIGN KEY (post_id) REFERENCES post(id),
@@ -48,30 +48,31 @@ CREATE TABLE favorites (
 
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE comment (
-                         id BINARY(36) PRIMARY KEY,
+                         id CHAR(36) PRIMARY KEY,
                          content VARCHAR(50) NOT NULL,
-                         post_id BINARY(16),
+                         post_id CHAR(36),
                          total_like INT NOT NULL,
                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                         created_by BINARY(36),
+                         created_by CHAR(36),
                          FOREIGN KEY (created_by) REFERENCES users(id),
                          FOREIGN KEY (post_id) REFERENCES post(id));
 
 
 DROP TABLE IF EXISTS `reactions`;
 CREATE TABLE reactions (
-                        id BINARY(36) PRIMARY KEY,
+                        id CHAR(36) PRIMARY KEY,
                         object_type VARCHAR(100) NOT NULL,
-                        object_id BINARY(36) NOT NULL,
+                        object_id CHAR(36) NOT NULL,
                         type INT NOT NULL,
-                        created_by BINARY(36) NOT NULL,
-                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+                        created_by CHAR(36) NOT NULL,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        FOREIGN KEY (created_by) REFERENCES users(id));
 
 
 DROP TABLE IF EXISTS `follows`;
 CREATE TABLE follows (
-                         following_user_id BINARY(16),
-                         user_id BINARY(16),
+                         following_user_id CHAR(36),
+                         user_id CHAR(36),
                          FOREIGN KEY (following_user_id) REFERENCES users(id),
                          FOREIGN KEY (user_id) REFERENCES users(id),
                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
