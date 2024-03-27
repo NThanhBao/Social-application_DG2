f4d2ba149362add2f370ee92a5033e80b58f2011
Thanh Bao <ntb09019@gmail.com>
2024-03-27 10:48:03 +0700

    update V3 flyway

--- src/main/resources/database/migration/V3__Insert_Data.sql
+++ src/main/resources/database/migration/V3__Insert_Data.sql
@@ -11,19 +11,19 @@ VALUES
     (UUID(), 'Second Post', 'This is the body of the second post.', 'published', 0, 0, (SELECT id FROM users WHERE username = 'jane_doe'));
 
 -- -- Chèn dữ liệu vào bảng medias
--- INSERT INTO medias (id, base_name, public_url, post_id)
--- VALUES
---     (UUID(), 'image1.jpg', 'http://example.com/image1.jpg', (SELECT id FROM post ORDER BY RANDOM() LIMIT 1)),
---     (UUID(), 'image2.jpg', 'http://example.com/image2.jpg', (SELECT id FROM post ORDER BY RANDOM() LIMIT 1));
+ INSERT INTO medias (id, base_name, public_url, post_id)
+ VALUES
+     (UUID(), 'image1.jpg', 'http://example.com/image1.jpg', (SELECT id FROM post ORDER BY RANDOM() LIMIT 1)),
+     (UUID(), 'image2.jpg', 'http://example.com/image2.jpg', (SELECT id FROM post ORDER BY RANDOM() LIMIT 1));
 
--- -- Chèn dữ liệu vào bảng favorites
--- INSERT INTO favorites (id, created_at, user_id)
--- VALUES
---     (UUID(), CURRENT_TIMESTAMP(), (SELECT id FROM users LIMIT 1)),
---     (UUID(), CURRENT_TIMESTAMP(), (SELECT id FROM users LIMIT 2));
+ -- Chèn dữ liệu vào bảng favorites
+ INSERT INTO favorites (id, created_at, user_id)
+ VALUES
+     (UUID(), CURRENT_TIMESTAMP(), (SELECT id FROM users LIMIT 1)),
+     (UUID(), CURRENT_TIMESTAMP(), (SELECT id FROM users LIMIT 2));
 
--- -- Chèn dữ liệu vào bảng reactions
--- INSERT INTO reactions (id, object_type, opject_id, type, create_by)
--- VALUES
---     (UUID(), 'post', (SELECT id FROM post LIMIT 1), 1, (SELECT id FROM users LIMIT 1)),
---     (UUID(), 'post', (SELECT id FROM post LIMIT 2), 2, (SELECT id FROM users LIMIT 2));
+ -- Chèn dữ liệu vào bảng reactions
+ INSERT INTO reactions (id, object_type, opject_id, type, create_by)
+ VALUES
+     (UUID(), 'post', (SELECT id FROM post LIMIT 1), 1, (SELECT id FROM users LIMIT 1)),
+     (UUID(), 'post', (SELECT id FROM post LIMIT 2), 2, (SELECT id FROM users LIMIT 2));
