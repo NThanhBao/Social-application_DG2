e74ad7dfcdb448229611675969b48fab8a7ac138
MidouHsk <huydung446@gmail.com>
2024-04-08 11:39:15 +0700

    update file JwtTokenUtil and JwtAuthenticationFilter

--- src/main/java/com/Social/application/DG2/util/annotation/LoginCheckAspect.java
+++ src/main/java/com/Social/application/DG2/util/annotation/LoginCheckAspect.java
@@ -1,16 +1,15 @@
 package com.Social.application.DG2.util.annotation;
 
 import com.Social.application.DG2.config.JwtTokenUtil;
-import com.Social.application.DG2.service.UsersService;
 import com.Social.application.DG2.util.exception.UnauthorizedException;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.MalformedJwtException;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -23,37 +22,18 @@ import jakarta.servlet.http.HttpServletRequest;
 public class LoginCheckAspect {
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
-    @Autowired
-    private UsersService customUserDetailsService;
-
     @Before("@annotation(CheckLogin)")
     public ResponseEntity<?> checkLogin( JoinPoint joinPoint) {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
         String authHeader = request.getHeader("Authorization");
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            throw new UnauthorizedException("Token not found or token does not exist.");
+            throw new UnauthorizedException("không tìm thấy token hoặc token không tồn tại.");
         }
-
-        String token = authHeader.substring(7);
-        String username = null;
-        try {
-            username = jwtTokenUtil.extractUsername(token);
-            if (username == null) {
-                throw new UnauthorizedException("Unable to extract username from token.");
-            }
-        } catch (Exception e) {
-            throw new UnauthorizedException(" "+ e.getMessage());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUserName = authentication.getName();
+            return ResponseEntity.ok(currentUserName);
         }
-        UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
-        try {
-            if (!jwtTokenUtil.validateToken(token, userDetails)) {
-                throw new UnauthorizedException("Authentication failed");
-            }
-        } catch (Exception e) {
-            throw new UnauthorizedException("Authentication failed: " + e.getMessage());
-        }
-        return ResponseEntity.ok().build();
+        throw new UnauthorizedException("Có lỗi sảy ra !");
     }
 }
-
-
